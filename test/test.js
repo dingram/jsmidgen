f807a5eb345d5ea33e971cb4142d14efabf6bde4
Dave Ingram <dave@dmi.me.uk>
2012-04-26 00:15:27 +0100

    Added instrument change shortcut and updated test to be more interesting

--- test/test.js
+++ test/test.js
@@ -14,10 +14,19 @@ track.addNote(0, 'a4', 64);
 track.addNote(0, 'b4', 64);
 track.addNote(0, 'c5', 64);
 
-track.addNoteOn(0, 'c4', 128);
+track.setInstrument(0, 0x13);
+
+track.addNoteOn(0, 'c4', 64);
 track.addNoteOn(0, 'e4');
 track.addNoteOn(0, 'g4');
-track.addNoteOff(0, 'c4', 128);
+track.addNoteOff(0, 'c4', 47);
+track.addNoteOff(0, 'e4');
+track.addNoteOff(0, 'g4');
+
+track.addNoteOn(0, 'c4', 1);
+track.addNoteOn(0, 'e4');
+track.addNoteOn(0, 'g4');
+track.addNoteOff(0, 'c4', 384);
 track.addNoteOff(0, 'e4');
 track.addNoteOff(0, 'g4');
 
