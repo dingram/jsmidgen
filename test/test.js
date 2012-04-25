40b9976be1ec7e9807c03d81e4b7ad63982ec891
Dave Ingram <dave@dmi.me.uk>
2012-04-25 23:58:54 +0100

    Now accept pitch names in preference to numbers

--- test/test.js
+++ test/test.js
@@ -5,20 +5,20 @@ var file = new Midi.File();
 var track = new Midi.Track();
 file.addTrack(track);
 
-track.addNote(0, 0x3C, 64);
-track.addNote(0, 0x3E, 64);
-track.addNote(0, 0x40, 64);
-track.addNote(0, 0x41, 64);
-track.addNote(0, 0x43, 64);
-track.addNote(0, 0x45, 64);
-track.addNote(0, 0x47, 64);
-track.addNote(0, 0x48, 64);
+track.addNote(0, 'c4', 64);
+track.addNote(0, 'd4', 64);
+track.addNote(0, 'e4', 64);
+track.addNote(0, 'f4', 64);
+track.addNote(0, 'g4', 64);
+track.addNote(0, 'a4', 64);
+track.addNote(0, 'b4', 64);
+track.addNote(0, 'c5', 64);
 
-track.addNoteOn(0, 0x3C, 128);
-track.addNoteOn(0, 0x40);
-track.addNoteOn(0, 0x43);
-track.addNoteOff(0, 0x3C, 128);
-track.addNoteOff(0, 0x40);
-track.addNoteOff(0, 0x43);
+track.addNoteOn(0, 'c4', 128);
+track.addNoteOn(0, 'e4');
+track.addNoteOn(0, 'g4');
+track.addNoteOff(0, 'c4', 128);
+track.addNoteOff(0, 'e4');
+track.addNoteOff(0, 'g4');
 
 fs.writeFileSync('test.mid', file.toBytes(), 'binary');
