4371a7f429c6b18a00486a415df15a1152f9499f
Walmik Deshpande <walmik.deshpande@gmail.com>
2015-12-29 13:58:09 -0800

    Set HDR_SPEED
    * Added setTicks functionality on File object
    * Added tests for valid & invalid input

---
+++ test/file-test.js
@@ -0,0 +1,21 @@
+var test = require('tape');
+var jsmidgen = require('../lib/jsmidgen.js');
+var file  = new jsmidgen.File();
+
+
+test('File -> setTicks should set the correct HDR_SPEED on valid input', function(t) {
+	file.setTicks(1000);
+	t.equal(file.HDR_SPEED, '\x03è', '1000 ticks should translate to \x03\xe8');
+
+	t.end();
+});
+
+test('File -> setTicks should return on invalid input', function(t) {
+	file.setTicks();
+	t.equal(file.HDR_SPEED, '\x03è', 'setTicks should return on empty input');
+
+	file.setTicks('not a number');
+	t.equal(file.HDR_SPEED, '\x03è', 'setTicks should return on invalid input');
+
+	t.end();
+});
