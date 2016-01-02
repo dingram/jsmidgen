8dbc8891789f7f55383a25e2477ed17eebdee2eb
Walmik Deshpande <walmik.deshpande@gmail.com>
2016-01-02 12:01:18 -0800

    Added test for invalid input

--- test/file-test.js
+++ test/file-test.js
@@ -3,8 +3,18 @@ var jsmidgen = require('../lib/jsmidgen.js');
 var file  = new jsmidgen.File();
 
 
-test('File -> setTicks', function(t) {
+test('File -> setTicks should set the correct HDR_SPEED on valid input', function(t) {
 	file.setTicks(1000);
+	t.equal(file.HDR_SPEED, '\\x03\\xe8', '1000 ticks should translate to \x03\xe8');
+
+	t.end();
+});
+
+test('File -> setTicks should return on invalid input', function(t) {
+	file.setTicks();
+	t.equal(file.HDR_SPEED, '\\x03\\xe8', '1000 ticks should translate to \x03\xe8');
+
+	file.setTicks('not a number');
 	t.equal(file.HDR_SPEED, '\\x03\\xe8', '1000 ticks should translate to \x03\xe8');
 
 	t.end();
