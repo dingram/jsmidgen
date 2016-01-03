d871eefdb02bde116d999cb32a2ab5e48aa0d630
Walmik Deshpande <walmik.deshpande@gmail.com>
2016-01-02 20:23:55 -0800

    Improved implementation and updated test

--- test/file-test.js
+++ test/file-test.js
@@ -4,8 +4,11 @@ var file  = new jsmidgen.File();
 
 
 test('File -> setTicks should set the correct HDR_SPEED on valid input', function(t) {
+	file.setTicks(128);
+	t.equal(file.HDR_SPEED, '\x00\x80', '128 ticks should translate to \x00\x80');
+
 	file.setTicks(1000);
-	t.equal(file.HDR_SPEED, '\x03è', '1000 ticks should translate to \x03\xe8');
+	t.equal(file.HDR_SPEED, '\x03\xe8', '1000 ticks should translate to \x03\xe8');
 
 	t.end();
 });
