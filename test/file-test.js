86ddec3e295b971cdbceb63d0128e5528714d985
Walmik Deshpande <walmik.deshpande@gmail.com>
2016-02-02 18:36:22 -0800

    Moved set ticks to File constructor via config object

--- test/file-test.js
+++ test/file-test.js
@@ -1,10 +1,10 @@
 var test = require('tape');
 var jsmidgen = require('../lib/jsmidgen.js');
-var file  = new jsmidgen.File();
-
 
 test('File -> setTicks should set the correct HDR_SPEED on valid input', function(t) {
-	file.setTicks(1000);
+	var file  = new jsmidgen.File({
+		ticks: 1000
+	});
 	t.equal(file.ticks, 1000, 'ticks should be set to 1000');
 
 	t.end();
@@ -12,13 +12,14 @@ test('File -> setTicks should set the correct HDR_SPEED on valid input', functio
 
 test('File -> setTicks should throw error on invalid input ', function(t) {
 	t.throws(function() {
-		file.setTicks()
+		new jsmidgen.File({
+			ticks: 'not a number'
+		});
 	});
 	t.throws(function() {
-		file.setTicks('not a number')
-	});
-	t.throws(function() {
-		file.setTicks(85000)
+		new jsmidgen.File({
+			ticks: 85000
+		});
 	});
 
 	t.end();
